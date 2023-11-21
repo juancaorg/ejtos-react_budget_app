8ec2e61414191847f4f837293ea0add6fbaffeb2
Juan Carrillo <juan@juanca.org>
2023-11-20 23:27:13 -0600

    feat(4): add currency prefix to the Change allocation textbox

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 const AllocationForm = (props) => {
-  const { dispatch, remaining } = useContext(AppContext);
+  const { dispatch, remaining, currency } = useContext(AppContext);
 
   const [name, setName] = useState("");
   const [cost, setCost] = useState("");
@@ -86,13 +86,13 @@ const AllocationForm = (props) => {
               Reduce
             </option>
           </select>
-
+          <span style={{ marginLeft: "1rem", marginRight: "1rem", fontSize: "1.5rem" }}>{currency}</span>
           <input
             required="required"
             type="number"
             id="cost"
             value={cost}
-            style={{ marginLeft: "2rem", size: 10 }}
+            style={{ size: 10 }}
             onChange={(event) => setCost(event.target.value)}
           ></input>
 

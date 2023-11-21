8c43b2c15534ba838508fcaf712505005da57039
Juan Carrillo <juan@juanca.org>
2023-11-21 08:16:28 -0600

    feat(3b): add styling to CurrencyDropdown

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -13,15 +13,19 @@ const Location = () => {
 
   return (
     <div className="alert alert-secondary">
-      <label for="currency-select">Currency:{" "}</label>
+      <label for="currency-select">Currency: </label>
       {
         <select
           name="currency"
           id="currency-select"
           onChange={(event) => changeCurrency(event.target.value)}
+          className="currency-select form-select form-select-sm"
+          style={{ backgroundColor: "#96e299", color: "white" }}
         >
           <option value="$">$ Dollar</option>
-          <option value="£" selected>£ Pound</option>
+          <option value="£" selected>
+            £ Pound
+          </option>
           <option value="€">€ Euro</option>
           <option value="₹">₹ Ruppee</option>
         </select>
